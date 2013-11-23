31f7f56c35d1b6810651ac2a517b3eba9455a138
Pablo Torres <tn.pablo@gmail.com>
2013-11-23 18:49:51 -0500

    Simplify router

--- lib/chuyo/router.rb
+++ lib/chuyo/router.rb
@@ -2,11 +2,11 @@ module Chuyo
   # Given a URL, return the callable that handles it.
   class Router
     def dispatch(request)
-      _, controller_name, action, after = request.path_info.split('/', 4)
+      _, controller, action, after = request.path_info.split('/', 4)
 
-      controller_name = "home" unless controller_name != ''
-      controller_name = param_to_controller_name(controller_name)
-      controller_klass = Object.const_get(controller_name)
+      controller = "home" unless controller != ''
+      controller = 'Controllers::' + controller.capitalize
+      controller_klass = Object.const_get(controller)
       controller = controller_klass.new(request)
 
       action = 'index' unless action && action != ''
@@ -14,12 +14,6 @@ module Chuyo
       controller.action(action)
     rescue NameError
       Controllers::Base.not_found
-    end
-
-    private
-
-    def param_to_controller_name(param)
-      'Controllers::' + param.capitalize
     end
   end
 end
